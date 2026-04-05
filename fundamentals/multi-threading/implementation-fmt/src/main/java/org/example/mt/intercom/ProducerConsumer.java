0d338ae2c55137b6a66da0e33af5842bc2d801f1
Pavan <pavankumarrouthu761@gmail.com>
2026-04-05 23:58:14 +0530

    multi threading basics

---
+++ fundamentals/multi-threading/implementation-fmt/src/main/java/org/example/mt/intercom/ProducerConsumer.java
@@ -0,0 +1,17 @@
+package org.example.mt.intercom;
+
+import org.example.mt.core.Kitchen;
+
+/**
+ * @author : Pavan Kumar
+ * @created : 05/04/26, Sunday
+ */
+
+public class ProducerConsumer {
+
+
+    public static void main(String[] args) {
+
+//        Thread producer = new Thread()
+    }
+}
